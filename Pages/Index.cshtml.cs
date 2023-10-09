872e3989a8fe28c05bf2a0d7a84839d688f01990
Caroline Boaventura <caroline.boaventura08@gmail.com>
2023-10-09 13:02:45 -0300

    Adicionando projeto inicial, com a parte inicial do front

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Estudos.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost(IFormFile file)
+    {
+        Console.WriteLine("entrou, agora sim");
+
+        Console.WriteLine(file);
+    }
+}
